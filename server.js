7b12b2e3594055431e9c535e19a503c58f6988c1
lmh0870 <lmh0870@naver.com>
2021-09-13 13:24:07 +0900

    Architecture: Part Two

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-import { ApolloServer, gql } from "apollo-server";
-import { typeDefs, resolvers } from "./schema";
+require("dotenv").config();
+import { ApolloServer } from "apollo-server";
+import schema from "./schema";
 
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+  schema,
 });
 
+const PORT = process.env.PORT;
+
 server
-  .listen()
-  .then(() => console.log("Server is running on http://localhost:4000/"));
+  .listen(PORT)
+  .then(() =>
+    console.log(`🚀Server is running on http://localhost:${PORT} ✅`)
+  );
